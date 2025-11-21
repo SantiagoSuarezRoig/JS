5b07595057dcc326113c554298b0bcfcbd6b5e46
SuarezRoig <santiagosuarez2604@gmail.com>
2025-11-21 15:12:33 +0100

    Create Class version of Cart

--- data/cart-class.js
+++ data/cart-class.js
@@ -2,24 +2,27 @@ import {priceOfDeliveryOption} from './deliveryOptions.js'
 import {productOfId} from './products.js'
 
 
+class Cart {
+    cartItems;
+    localStorageKey;
 
-function Cart(localStorageKey){
-    const cart = {
-    cartItems: undefined,
+    constructor(localStorageKey){
+        this.localStorageKey = localStorageKey;
+        this.loadCartFromStorage()
+    }
 
     loadCartFromStorage(){
-        this.cartItems = JSON.parse(localStorage.getItem(localStorageKey))
+        this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey))
 
         if(!this.cartItems) this.cartItems = [
         {productId:"c2a82c5e-aff4-435f-9975-517cfaba2ece" , quantity:1, deliveryOptionId:'1'},
         {productId:"6b07d4e7-f540-454e-8a1e-363f25dbae7d", quantity:1, deliveryOptionId:'2'}  
         ];
-    },
+    }
 
     saveToStorage(){
-        localStorage.setItem(localStorageKey,JSON.stringify(this.cartItems))
-    },
-
+        localStorage.setItem(this.localStorageKey,JSON.stringify(this.cartItems))
+    }
 
     addToCart(productId,quantity){
         let i = 0
@@ -30,14 +33,14 @@ function Cart(localStorageKey){
         else this.cartItems.push({productId, quantity, deliveryOptionId:'1'})
         
         this.saveToStorage()
-    },
+    }
 
     calculateHandlingMoney(){
-        let total = 0;
-        this.cartItems.forEach(cartItem =>
-            total += priceOfDeliveryOption(cartItem.deliveryOptionId))
-        return total
-    },
+    let total = 0;
+    this.cartItems.forEach(cartItem =>
+        total += priceOfDeliveryOption(cartItem.deliveryOptionId))
+    return total
+    }
 
     calculateCartTotalMoney(){
         let total = 0;
@@ -45,14 +48,14 @@ function Cart(localStorageKey){
             total += cartItem.quantity * productOfId(cartItem.productId).priceCents
         )
         return total
-    },
+    }
 
     calculateCartQuantity(){
         let total = 0
         this.cartItems.forEach(cartItem => total+= cartItem.quantity)
 
         return total
-    },
+    }
 
     removeCartItem(id){
         this.cartItems = this.cartItems.filter(cartitem=> cartitem.productId !== id)
@@ -61,7 +64,7 @@ function Cart(localStorageKey){
             document.querySelector(`.js-item-container-${id}`).remove()
 
         this.saveToStorage()
-    },
+    }
 
     changeQuantityOfProduct(productId,quantity){
         let i = 0
@@ -70,7 +73,7 @@ function Cart(localStorageKey){
         this.cartItems[i].quantity = quantity
         
         this.saveToStorage()
-    },
+    }
 
     updateDeliveryOption(productId,deliveryOption){
         if(deliveryOption != '1' && deliveryOption != '2'&& deliveryOption != '3')
@@ -88,40 +91,16 @@ function Cart(localStorageKey){
         this.saveToStorage()
     }
 
-    }
-
-    return cart
 }
 
 
-let cartPijin = Cart('cartPijin')
-let cartBusiness = Cart('cartBusinnes')
 
 
-cartPijin.loadCartFromStorage()
-cartBusiness.loadCartFromStorage()
+let cartPijin = new Cart('cart-Pijin')
+let cartBusiness = new Cart('cart-Pijin')
+
 
 cartPijin.addToCart('b0f17cc5-8b40-4ca5-9142-b61fe3d98c85',1)
-
 console.log(cartPijin)
 console.log(cartBusiness)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(cartPijin instanceof Cart)
